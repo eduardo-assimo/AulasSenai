e44d0cc52775061236918720ed4d3fe70ba6e8d2
eduardoassimo <eduardo.assimo@gmail.com>
2021-10-07 22:17:34 -0300

    tarefa da calculadora

---
+++ calculadora.js
@@ -0,0 +1,10 @@
+var number1 = 10;
+var number2 = 2;
+var soma = number1 + number2;
+var subtracao = number1 - number2;
+var multiplicacao = number1 * number2;
+var divisao = number1 / number2;
+console.log(soma);
+console.log(subtracao);
+console.log(multiplicacao);
+console.log(divisao);
